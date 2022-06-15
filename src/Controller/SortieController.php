fcef4e88b132037dfa6b52a12484eb18a29ddd15
Philéas PERON <phileas.peron2022@campus-eni.fr>
2022-06-15 09:24:01 +0200

    crud+relations entities

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/sortie")
+ */
+class SortieController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_sortie_index", methods={"GET"})
+     */
+    public function index(SortieRepository $sortieRepository): Response
+    {
+        return $this->render('sortie/index.html.twig', [
+            'sorties' => $sortieRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_sortie_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, SortieRepository $sortieRepository): Response
+    {
+        $sortie = new Sortie();
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sortieRepository->add($sortie, true);
+
+            return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('sortie/new.html.twig', [
+            'sortie' => $sortie,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_sortie_show", methods={"GET"})
+     */
+    public function show(Sortie $sortie): Response
+    {
+        return $this->render('sortie/show.html.twig', [
+            'sortie' => $sortie,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_sortie_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
+    {
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sortieRepository->add($sortie, true);
+
+            return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('sortie/edit.html.twig', [
+            'sortie' => $sortie,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_sortie_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$sortie->getId(), $request->request->get('_token'))) {
+            $sortieRepository->remove($sortie, true);
+        }
+
+        return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Controller/VilleController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ville;
+use App\Form\VilleType;
+use App\Repository\VilleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/ville")
+ */
+class VilleController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_ville_index", methods={"GET"})
+     */
+    public function index(VilleRepository $villeRepository): Response
+    {
+        return $this->render('ville/index.html.twig', [
+            'villes' => $villeRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_ville_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, VilleRepository $villeRepository): Response
+    {
+        $ville = new Ville();
+        $form = $this->createForm(VilleType::class, $ville);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $villeRepository->add($ville, true);
+
+            return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('ville/new.html.twig', [
+            'ville' => $ville,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_ville_show", methods={"GET"})
+     */
+    public function show(Ville $ville): Response
+    {
+        return $this->render('ville/show.html.twig', [
+            'ville' => $ville,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_ville_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Ville $ville, VilleRepository $villeRepository): Response
+    {
+        $form = $this->createForm(VilleType::class, $ville);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $villeRepository->add($ville, true);
+
+            return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('ville/edit.html.twig', [
+            'ville' => $ville,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_ville_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Ville $ville, VilleRepository $villeRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ville->getId(), $request->request->get('_token'))) {
+            $villeRepository->remove($ville, true);
+        }
+
+        return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
